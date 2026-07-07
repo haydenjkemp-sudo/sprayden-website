650e6682f469725957183fe336e2785b509555b6
haydenjkemp-sudo <haydenjkemp@gmail.com>
2026-07-07 22:36:40 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const slider = document.getElementById("compare");
-const afterWrapper = document.querySelector(".after-wrapper");
-
-if (slider && afterWrapper) {
-  slider.addEventListener("input", function () {
-    afterWrapper.style.width = slider.value + "%";
-  });
-}
